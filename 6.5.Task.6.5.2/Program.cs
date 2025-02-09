6026b5a85ed673c289bb257810ab07401c164c8a
Timosha-go-up <r.o4agow2010@yandex.ru>
2025-02-10 03:49:27 +0500

    6.5.Task.6.5.2/Program.cs

--- 6.5.Task.6.5.2/Program.cs
+++ 6.5.Task.6.5.2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _6._5.Task._6._5._2
 {
@@ -14,7 +10,60 @@ namespace _6._5.Task._6._5._2
         static void Main(string[] args)
         {
 
+            Triangle triangle = new Triangle();   
+            
+            Console.WriteLine("Triengle: {0:0.##}", triangle.Square(2, 2, 2));
+
+            Circle circle = new Circle();
+
+            Console.WriteLine("Circle:   {0:0.##}", circle.Square(2));
+
+            Quadrate quadrate = new Quadrate();
+            Console.WriteLine("Quadrate: {0:0.##}",quadrate.Square(2, 4));
+
         }
+
+        class Triangle
+        {
+            public int x;
+            public int y;
+            public int z;
+
+            public double Square(int x, int y, int z)
+            {
+                int rez = (x + y + z) / 2;
+                rez *= (rez - x) * (rez - y) * (rez - z);
+                double result = Math.Sqrt(rez);
+
+                return result;
+            }
+
+        }
+
+        class Circle
+        { 
+            public int R;
+
+            public double Square(int r) 
+            {
+                return r * r * 3.14;
+            }
+        }
+
+        class Quadrate
+        {
+            public int a;
+            public int b;
+            public int Square(int a, int b)
+            { return a * b; }
+        
+        }
+
+       
+
+        
+
+
 
     }
 }
