c7ad82f662fb8093c366c830571f4109bc5afb6c
Timosha-go-up <r.o4agow2010@yandex.ru>
2025-02-10 01:10:49 +0500

    6.3.reference.and.significant.types.data update

--- 6.3.reference.and.significant.types.data/Program.cs
+++ 6.3.reference.and.significant.types.data/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _6._3.reference.and.significant.types.data
 {
@@ -11,6 +7,14 @@ namespace _6._3.reference.and.significant.types.data
         static void Main(string[] args)
         {
             _ = GetCurrentDepartment("Банк", "Санкт-Петербург");
+
+            Console.WriteLine();
+
+            _ = GetCurrentDepartment(null, "Санкт-Петербург",null);
+
+            Console.WriteLine();
+
+            _ = GetCurrentDepartment("Bank", null, "Kingston");
 
             Console.ReadKey();
         }
@@ -50,9 +54,28 @@ namespace _6._3.reference.and.significant.types.data
             return department;
         }
 
-    //Напишите такой код, который бы при типе компании, равному типу "Банк", и городе "Санкт-Петербург" 
-    //выводил в консоль сообщение "У банка ??? есть отделение в Санкт-Петербурге", где вместо "???" выводилось бы название компании.
-    //Если у компании нет названия, вместо него должно быть "Неизвестная компания".
+
+        static Department GetCurrentDepartment(string CompanyType, string CityName,string CompanyName)
+        {
+            Department department = new Department
+            {
+                City = new City(),
+                Company = new Company()
+            };
+            string str = "no data";            
+            department.City.Name = CityName;
+            department.Company.Type = CompanyType;
+            department.Company.Name = CompanyName;
+
+            Console.WriteLine($"Тип компании - '{department?.Company?.Type ?? str}' Название города - '{department?.City?.Name ?? str}'  Название компании - '{department?.Company?.Name ?? str}'");
+            
+            return department;
+        }
+
+
+        //Напишите такой код, который бы при типе компании, равному типу "Банк", и городе "Санкт-Петербург" 
+        //выводил в консоль сообщение "У банка ??? есть отделение в Санкт-Петербурге", где вместо "???" выводилось бы название компании.
+        //Если у компании нет названия, вместо него должно быть "Неизвестная компания".
 
 
 
